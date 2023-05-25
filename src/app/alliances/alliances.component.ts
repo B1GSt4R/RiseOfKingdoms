e7443e1eb7d38afb36c7de14298600aa84dccb48
Dominic <B1GSt4RBusiness@gmail.com>
2023-05-25 22:33:27 +0200

    table

--- src/app/alliances/alliances.component.ts
+++ src/app/alliances/alliances.component.ts
@@ -1,21 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { TreeNode } from 'primeng/api';
-import { HttpClient } from '@angular/common/http';
-import { ActivatedRoute, RouterLinkActive } from '@angular/router';
+import { Component } from '@angular/core';
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
 
 @Component({
   selector: 'app-alliances',
   templateUrl: './alliances.component.html',
   styleUrls: ['./alliances.component.scss'],
 })
-export class AlliancesComponent implements OnInit {
-  data: TreeNode[] = [];
-
-  constructor(private http:HttpClient, private route:ActivatedRoute) {}
-
-  async ngOnInit() {
-    const id = String(this.route.snapshot.paramMap.get("id"));
-    var res:any = await this.http.get('/assets/'+id+'.json').toPromise();
-    this.data = res;
-  }
+export class AlliancesComponent {
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
 }
